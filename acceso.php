7c23fcac8098880f895cdb28beec5c8d63d727d9
Gianfranco Nicolás <gianfrancopr08@gmail.com>
2024-11-02 14:09:03 -0500

    Verificación de credenciales y creación de cookies.

---
+++ acceso.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+
+    $usuario = $_POST["usuario"];
+    $clave = $_POST["clave"];
+
+    if ($usuario == "Nico" && $clave == "1234") {
+        $_SESSION["usuario"] = $usuario;
+        $_SESSION["clave"] = $clave;
+
+        if(isset($_POST["recordarme"])) {
+            setcookie("c_usuario",$usuario, 0);
+            setcookie("c_clave", $clave, 0);
+        }else{
+            setcookie("c_usuario","");
+            setcookie("c_clave","");
+        }
+
+        header("Location:panel.php");
+    } else{
+        header("Location:login.php");
+    }
+ 
+
+?>

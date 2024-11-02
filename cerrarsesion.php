bd655b126666ae806d60f6425a85a5d889f4c606
Gianfranco Nicolás <gianfrancopr08@gmail.com>
2024-11-02 14:09:52 -0500

    Eliminar la sesión y resetear las cookies.

---
+++ cerrarsesion.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+    session_destroy();
+
+    if(!isset($_COOKIE["c_usuario"]) && !isset($_COOKIE["c_clave"])){
+        setcookie("c_usuario","");
+        setcookie("c_clave","");
+        setcookie("c_idioma", "");
+    }
+
+
+
+    header("Location:login.php");
+
+?>
